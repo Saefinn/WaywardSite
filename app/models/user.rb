86e59ba87dc2fb2557f5eadca765143ffaa3877d
Saefinn <bryn.s.price@live.co.uk>
2015-04-20 23:37:14 +0100

    Issue with adding names fixed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
+  has_many :statuses
   #attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :profile_name
+  def full_name
+    first_name + " " + last_name
+    #code
+  end
 end
