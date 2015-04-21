611abd563d881ee2ac5cc31bb9f6a4243e617e7e
Saefinn <bryn.s.price@live.co.uk>
2015-04-21 14:41:56 +0100

    Updated with Validation on sign up and added tests for username, lastname and profile correctness

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,15 @@ class User < ActiveRecord::Base
   has_many :statuses
   #attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :profile_name
   
+  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :profile_name, presence: true, uniqueness: true, format: {
+    with: /\A[a-zA-Z\-\_]+\Z/, message: 'Must be formatted correctly.'
+  }
+  
+  
+  
   #Combine the first and last name to give a full name
   def full_name
     first_name + " " + last_name
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,43 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "a user should enter a first name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:first_name].empty?
+  end
+  
+  test "a user should enter a last name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:last_name].empty?
+  end
+  
+  test "a user should enter a profile name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+  end
+  
+  test "a user should have a unique profile name" do
+    user = User.new
+    user.profile_name = 'saefinn'
+    user.email = "bryn.price@live.co.uk"
+    user.first_name = 'bryn'
+    user.last_name = 'price'
+    user.password = "password"
+    user.password_confirmation = "password"
+    assert !user.save
+    puts user.errors.inspect
+    assert !user.errors[:profile_name].empty?
+  end
+  
+  test "a user should have a profile name without spaces" do
+    user = user.new
+    user.profile_name = "My Profile with spaces"
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+    assert user.errors[:profile_name].include?("Must be formatted correctly.")
+  end
+  
 end
