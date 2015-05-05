2e5fda0a7b4969c7342edc4c97351d3e7ed38057
Saefinn <bryn.s.price@live.co.uk>
2015-05-05 21:04:59 +0100

    Updated with a RubyMine Project

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,9 @@
 class Character < ActiveRecord::Base
+  belongs_to :user
+
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true, length: { minimum: 2}
+
+
+
 end
