98573133f357b365e230940f5f88261e8da58fb0
Saefinn <bryn.s.price@live.co.uk>
2016-11-11 13:09:14 +0000

    Added a new text editor for BBCode and fixed some post CSS issues, where added a tag could mean it doesn't go to the next line.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,3 +26,37 @@ $(document).on('page:change', function(){
     
     
 });
+
+
+mySettings = {
+  nameSpace:          "bbcode", // Useful to prevent multi-instances CSS conflict
+  previewParserPath:  "~/sets/bbcode/preview.php",
+  markupSet: [
+      {name:'B', key:'B', openWith:'[b]', closeWith:'[/b]'}, 
+      {name:'I', key:'I', openWith:'[i]', closeWith:'[/i]'}, 
+      {name:'U', key:'U', openWith:'[u]', closeWith:'[/u]'},
+      {name:'Del', openWith:'[del]', closeWith:'[/del]'},
+      {name:'Insert', openWith:'[ins]', closeWith:'[/ins]'}, 
+      {separator: ' | '},
+      {name:'Picture', key:'P', replaceWith:'[img][![Url]!][/img]'},
+      {name:'YouTube', openWith:'[youtube]', closeWith:'[/youtube]'}, 
+      {name:'Link', key:'L', openWith:'[url=[![Url]!]]', closeWith:'[/url]', placeHolder:'Your text to link here...'},
+      {separator: ' | Text Size: '},
+      {name:'Big', openWith:'[size=48]', closeWith:'[/size]' },
+	  {name:'Medium', openWith:'[size=20]', closeWith:'[/size]' },
+      {name:'Small', openWith:'[size=10]', closeWith:'[/size]' },
+      {separator: ' | '},
+      {name:'Bulleted list', openWith:'[ul]\n', closeWith:'\n[/ul]'}, 
+      {name:'Numeric list', openWith:'[ol]]\n', closeWith:'\n[/ol]'}, 
+      {name:'List item', openWith:'[li] ', closeWith: '[/li]'}, 
+      {separator: ' | '},
+      {name:'Quotes', openWith:'[quote]', closeWith:'[/quote]'}, 
+      {name:'Code', openWith:'[code]', closeWith:'[/code]'}, 
+      {separator:' | ' },
+      {name:'Clean', className:"clean", replaceWith:function(h) { return h.selection.replace(/\[(.*?)\]/g, "") } },
+      
+   ]
+}
+$(document).on('page:change', function() {
+   $("textarea").markItUp(mySettings);
+});
